c87eab8a342f901b119429195b935c48c9d99f6a
lirt <540491950@qq.com>
2019-12-08 21:15:04 +0800

    主页面左边导航菜单

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,43 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home_container">
+    <el-header>
+      <div>
+        <img src="../assets/logo.png" width="40" height="40" alt="">
+        <span>XXX系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <!--侧边栏菜单区域-->
+        <el-menu
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b">
+          <!--一级菜单-->
+          <el-submenu index="1">
+            <!--一级菜单模板区域-->
+            <template slot="title">
+              <!--图标-->
+              <i class="el-icon-location"></i>
+              <!--文本-->
+              <span>导航一</span>
+            </template>
+            <!--二级菜单-->
+            <el-menu-item index="1-4-1">
+              <template slot="title">
+                <!--图标-->
+                <i class="el-icon-location"></i>
+                <!--文字-->
+                <span>导航一</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -15,6 +51,35 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="less">
+  .home_container {
+    height: 100%;
+  }
 
+  .el-header {
+    background-color: #373D41;
+    display: flex;
+    justify-content: space-between;
+    padding-left: 0;
+    align-items: center;
+    color: #ffffff;
+    font-size: 20px;
+
+    div {
+      display: flex;
+      align-items: center;
+
+      span {
+        margin-left: 15px;
+      }
+    }
+  }
+
+  .el-aside {
+    background-color: #333744;
+  }
+
+  .el-main {
+    background-color: #EAEDF1;
+  }
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -4,11 +4,27 @@ import {
   Form,
   FormItem,
   Input,
-  Message
+  Message,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Menu,
+  Submenu,
+  MenuItemGroup,
+  MenuItem
 } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItemGroup)
+Vue.use(MenuItem)
 Vue.prototype.$message = Message
